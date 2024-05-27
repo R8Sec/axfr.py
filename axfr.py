90b236fda2ec7ed0aa12b547bb4f1573f5f0f0b0
torm3n <you@example.com>
2024-05-27 20:04:34 +0100

    working on output

--- axfr.py
+++ axfr.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-import dns.zone as dz
+import argparse
 import dns.query as dq
-import dns.resolver as dr
 import dns.rdataclass
 import dns.rdatatype
-import argparse
+import dns.resolver as dr
+import dns.zone as dz
 import sys
 
 # Function to find name servers
@@ -37,35 +37,36 @@ def find_nameservers(domain):
         return nameserver_ips
 
 # Make the output more readable
-def print_zone_data(zone):
-    # Print zone data
-    for name, node in zone.nodes.items():   # Iterate over the nodes in the zone
-        print(f'\n{name}:')   # Print the name of the node
-        for rdataset in node.rdatasets: # Iterate over the rdatasets in the node
-            # Print the rdataset details
-            print(f"{name}.{zone.origin} {rdataset.ttl} {dns.rdataclass.to_text(rdataset.rdclass)} {dns.rdatatype.to_text(rdataset.rdtype)}")
-            for rdata in rdataset:
-                print(f' --> {rdata}')
-
-# Gather found subdomains after successful zone transfer
-def get_subdomains(axfr, domain):
-    subdomains = []   # List to store tuple containing subdomain and ip address
-    for record in axfr:
-        subdomains.append(f'{record.to_text()}.{domain}')
-
-    return subdomains
+def print_zone_data(zone_data):
+    for record in sorted(zone_data):
+        print(record)
 
 def zone_transfer(domain, nameserver, timeout):
     try:    # Try zone transfer
         zone = dz.from_xfr(dq.xfr(nameserver, domain, lifetime=timeout))
+        zone_data = set()   # Set to store zone data
+        for name, node in zone.nodes.items():   # Iterate over the nodes in the zone
+            for rdataset in node.rdatasets: # Iterate over the rdatasets in the node
+                for rdata in rdataset:
+                    record = f"{name}.{zone.origin} {rdataset.ttl} {dns.rdataclass.to_text(rdataset.rdclass)} {dns.rdatatype.to_text(rdataset.rdtype)} {rdata}"
+                    zone_data.add(record)
+        print(f'[+] Successful zone transfer from {nameserver}')
+        return zone_data
 
     # Print error if zone transfer fails and exit
     except Exception as error:
         print(f'[-] Zone transfer failed from {nameserver}.\nError: {error}')
         sys.exit(1)
 
-    print(f'\n[*] Successful zone transfer from {nameserver}')
-    return zone
+def setup_nameservers(nameservers, domain):
+    try:
+        if nameservers:
+            return [ip.strip() for ip in nameservers.split(',')]
+        else:
+            return find_nameservers(domain)
+    except Exception as error:
+        print(error)
+        sys.exit(1)
 
 # Main
 def main():
@@ -79,12 +80,6 @@ def main():
         default=None,
         help='List of nameserver IPs (e.g., "8.8.8.8, 8.8.4.4, 1.1.1.1")',
     )
-    parser.add_argument('-s',
-        '--subdomains',
-        action='store_true',
-        required=False,
-        help='Output only the found subdomains. Don\'t show details',
-    )
     args = parser.parse_args()
 
     # Initialise dns.resolver.Resolver class
@@ -95,28 +90,34 @@ def main():
     timeout = args.timeout  # Timeout for DNS queries
     nameservers = args.nameservers  # Nameserver IPs
 
-    # Setup name servers
-    try:
-        if not nameservers:
-            r.nameservers = find_nameservers(domain)
-        else:
-            r.nameservers = [ip.strip() for ip in nameservers.split(',')]
-    except Exception as error:
-        print(error)
-        sys.exit(1)
+    # Setup nameservers
+    r.nameservers = setup_nameservers(nameservers, domain)
 
     # Try zone transfer
-    subdomains = []
+    all_zone_data = []
     for nameserver in r.nameservers:
-        axfr = zone_transfer(domain, nameserver, timeout)
-        if args.subdomains:
-            subdomains += get_subdomains(axfr, domain)
-        else:
-            print_zone_data(axfr)
+        zone_data = zone_transfer(domain, nameserver, timeout)
+        all_zone_data.append(zone_data)
 
-    if args.subdomains:
-        unique_subdomains = set(subdomains)
-        [print(subdomain) for subdomain in unique_subdomains]
+    unique_zone_data = list(set(frozenset(zone) for zone in all_zone_data))
+    print("Zone Transfer Results:\n")
+    for i, zone_data in enumerate(unique_zone_data):
+        print(f"Zone transfer from {r.nameservers[i]}:\n")
+        print_zone_data(zone_data)
+        print("\n")
+
+    if len(unique_zone_data) > 1:
+        print("Differences between zone transfer results found:\n")
+        base_data = unique_zone_data[0]
+        for other_data in unique_zone_data[1:]:
+            differences = base_data.symmetric_difference(other_data)
+            if differences:
+                print("Differences found:\n")
+                print_zone_data(differences)
+                print("\n")
 
 if __name__=='__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n[!] Exiting program")
